a65235aaa5f493abaf5511246b0bee0e7ad8594e
Prashanth Kampli <107469003+prashanthkampli@users.noreply.github.com>
2024-06-21 14:05:09 +0530

    2nd Program.py

---
+++ 2nd Program.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+
+# Loading Data from a CSV File
+data = pd.DataFrame(data=pd.read_csv(r"C:\Users\sruja\MachineLearning\pythonProject\candidate.csv"))
+
+# Separating concept features from Target
+concepts = np.array(data.iloc[:, 0:-1])
+
+# Isolating target into a separate DataFrame
+# copying last column to target array
+target = np.array(data.iloc[:, -1])
+
+
+def learn(concepts, target):
+    specific_h = concepts[0].copy()
+    print("initialization of specific_h and general_h")
+    print(specific_h)
+
+    general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
+    print(general_h)
+
+    for i, h in enumerate(concepts):
+
+        if target[i] == "Yes":
+            for x in range(len(specific_h)):
+
+                if h[x] != specific_h[x]:
+                    specific_h[x] = '?'
+                    general_h[x][x] = '?'
+
+
+        if target[i] == "No":
+            for x in range(len(specific_h)):
+                # For negative hypothesis change values only in G
+                if h[x] != specific_h[x]:
+                    general_h[x][x] = specific_h[x]
+                else:
+                    general_h[x][x] = '?'
+
+        print("steps of Candidate Elimination Algorithm", i + 1)
+        print(specific_h)
+        print(general_h)
+
+
+    indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
+    for i in indices:
+        # remove those rows from general_h
+        general_h.remove(['?', '?', '?', '?', '?', '?'])
+
+    return specific_h, general_h
+
+
+s_final, g_final = learn(concepts, target)
+print("Final Specific_h:", s_final, sep="\n")
+print("Final General_h:", g_final, sep="\n")
