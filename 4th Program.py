36c8d7bcd40f9456dd84c346fefee66bb243595d
Prashanth Kampli <107469003+prashanthkampli@users.noreply.github.com>
2024-08-05 19:41:08 +0530

    4th Program

--- 4th Program.py
+++ 4th Program.py
@@ -1,17 +1,12 @@
 import numpy as np
 
-
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size, learning_rate):
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
         self.learning_rate = learning_rate
-
-        self.weights_input_hidden = np.random.randn(self.input_size, self.hidden_size)
-        self.bias_input_hidden = np.random.randn(1, self.hidden_size)
-        self.weights_hidden_output = np.random.randn(self.hidden_size, self.output_size)
-        self.bias_hidden_output = np.random.randn(1, self.output_size)
+        self.weights_input_hidden = np.random.randn(input_size, hidden_size)
+        self.bias_input_hidden = np.random.randn(1, hidden_size)
+        self.weights_hidden_output = np.random.randn(hidden_size, output_size)
+        self.bias_hidden_output = np.random.randn(1, output_size)
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -28,14 +23,13 @@ class NeuralNetwork:
         output_delta = output_error * self.sigmoid_derivative(self.output)
         hidden_error = output_delta.dot(self.weights_hidden_output.T)
         hidden_delta = hidden_error * self.sigmoid_derivative(self.hidden_output)
-
         self.weights_hidden_output += self.hidden_output.T.dot(output_delta) * self.learning_rate
         self.bias_hidden_output += np.sum(output_delta, axis=0, keepdims=True) * self.learning_rate
         self.weights_input_hidden += inputs.T.dot(hidden_delta) * self.learning_rate
         self.bias_input_hidden += np.sum(hidden_delta, axis=0, keepdims=True) * self.learning_rate
 
     def train(self, inputs, targets, epochs):
-        for epoch in range(epochs):
+        for _ in range(epochs):
             self.forward(inputs)
             self.backward(inputs, targets)
 
@@ -43,20 +37,11 @@ class NeuralNetwork:
         self.forward(inputs)
         return self.output
 
-
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 targets = np.array([[0], [1], [1], [0]])
 
-input_size = 2
-hidden_size = 3
-output_size = 1
-learning_rate = 0.1
-epochs = 10000
-
-nn = NeuralNetwork(input_size, hidden_size, output_size, learning_rate)
-nn.train(inputs, targets, epochs)
-
-predictions = nn.predict(inputs)
+nn = NeuralNetwork(input_size=2, hidden_size=3, output_size=1, learning_rate=0.1)
+nn.train(inputs, targets, epochs=10000)
 
 print("Predictions:")
-print(predictions)
+print(nn.predict(inputs))
