45beb24bbfafbfa17bc444d8454aab9d2bd1f86f
Prashanth Kampli <107469003+prashanthkampli@users.noreply.github.com>
2024-08-05 19:41:45 +0530

    5th Program

--- 5th Program.py
+++ 5th Program.py
@@ -9,19 +9,11 @@ newsgroups_test = fetch_20newsgroups(subset='test')
 vectorizer = CountVectorizer()
 X_train = vectorizer.fit_transform(newsgroups_train.data)
 X_test = vectorizer.transform(newsgroups_test.data)
+y_train, y_test = newsgroups_train.target, newsgroups_test.target
 
-y_train = newsgroups_train.target
-y_test = newsgroups_test.target
-
-nb_classifier = MultinomialNB()
-nb_classifier.fit(X_train, y_train)
-
+nb_classifier = MultinomialNB().fit(X_train, y_train)
 y_pred = nb_classifier.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='weighted')
-recall = recall_score(y_test, y_pred, average='weighted')
-
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average='weighted'))
+print("Recall:", recall_score(y_test, y_pred, average='weighted'))
